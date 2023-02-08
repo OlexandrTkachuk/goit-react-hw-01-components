4b743578b97d9039ef3dea9924861e19dd2be18c
Olexandr Tkachuk <temishena@gmail.com>
2023-02-08 19:54:35 +0200

    hahaha

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Profile from 'components/Profile';
 import Statistics from 'components/Statistics';
 import FriendItemList from 'components/FriendItemList';
+import TransactionHistory from 'components/TransactionHistory';
 
 import user from 'user.json';
 import data from 'data.json';
 import friends from 'friends.json';
+import transactions from 'transactions.json';
 
 export default function App() {
   return (
@@ -22,6 +24,7 @@ export default function App() {
 
       <Statistics stats={data} />
       <FriendItemList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
