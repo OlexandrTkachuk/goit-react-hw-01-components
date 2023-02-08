cf8f1d0462ae4716e2a616ad538b3a4de1469861
Olexandr Tkachuk <temishena@gmail.com>
2023-02-08 15:49:41 +0200

    1

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ export default function App() {
           likes={user.stats.likes}
         />
       </Section>
+      {/* <Section></Section>
       <Section></Section>
-      <Section></Section>
-      <Section></Section>
+      <Section></Section> */}
     </div>
   );
 }
